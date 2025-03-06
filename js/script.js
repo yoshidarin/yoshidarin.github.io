c0e3617010993bd9d96e33951dcc608bdb930206
yoshidarin <rin.yoshida@itpm-gk.com>
2025-03-07 08:34:44 +0900

    ローディング画面追加　おみくじアプリ詳細ページ追加

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+// ローディング画面制御
+const loading = document.querySelector(".loading_bg");
+
+// ページの読み込み完了時に処理を実行
+window.addEventListener("load", () => {
+    // 3秒後にローディング画面を非表示にする
+    setTimeout(() => {
+        loading.classList.add("loaded");
+    }, 3000);
+});
