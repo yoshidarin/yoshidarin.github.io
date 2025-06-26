8800c8ed5d8f6a3ddb379ed04f034250fddd24d0
yoshidarin <rin.yoshida@itpm-gk.com>
2025-06-26 16:27:05 +0900

    アプリ詳細ページ作成

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const loading = document.querySelector(".loading_bg");
 
 // ページの読み込み完了時に処理を実行
 window.addEventListener("load", () => {
-    // 3秒後にローディング画面を非表示にする
+    // 1.5秒後にローディング画面を非表示にする
     setTimeout(() => {
         loading.classList.add("loaded");
-    }, 3000);
+    }, 1500);
 });
